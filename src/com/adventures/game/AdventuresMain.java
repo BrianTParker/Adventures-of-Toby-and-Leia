09d8f7b5fa06de70aa1fa6f389da62db45c5c392
MisterToby <brithom.parker@gmail.com>
2015-08-02 21:14:08 -0400

    Initial Commit

---
+++ src/com/adventures/game/AdventuresMain.java
@@ -0,0 +1,17 @@
+package com.adventures.game;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+
+
+public class AdventuresMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		 new LwjglApplication(new AdventuresGame(), "Game", 480, 320);
+	}
+
+}
